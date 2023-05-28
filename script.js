866776d3e37e6abe7cf9bc1b53adfdf1a73f0adb
Aditya sen <130759840+Adi91108@users.noreply.github.com>
2023-05-28 14:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,14 +11,27 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+   let decodedArr = ""; // Your Result goes here
   // Only change code below this line
+	for(let i=0;i<encodedStr.length;i++)
+		{
+			if(lookup[encodedStr.charAt(i)]===undefined)
+			{
+				decodedArr+=encodedStr.charAt(i);
+			}
+			else
+			{
+				decodedArr+=lookup[encodedStr.charAt(i)];
+			}
+			
+		}
 
-  return ;//return decodedArr
+ // return ;
+	return decodedArr;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+ //console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
